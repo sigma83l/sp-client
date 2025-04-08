87f6f7a852d69c12920dc73a2f214d28d2ddc89c
sigma83l <hamedsedaghatgit83@gmail.com>
2025-04-08 12:15:26 -0700

    minor fix 2

---
+++ utils/cookies.js
@@ -0,0 +1,10 @@
+import cookie from 'cookie';
+
+/**
+ * Function to parse cookies from the request headers
+ * @param {Object} req - The Next.js request object
+ * @returns {Object} - An object containing cookies
+ */
+export function parseCookies(req) {
+  return cookie.parse(req ? req.headers.cookie || "" : document.cookie);
+}
